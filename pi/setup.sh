bb815be553960f08c8852edc3c854167759ea2ac
Zero Cho <itszero@gmail.com>
2020-11-23 20:02:21 -0800

    supervisord

--- pi/setup.sh
+++ pi/setup.sh
@@ -4,3 +4,6 @@ echo "-> Make a 4G image for the drive"
 fallocate -l 4G drive.img
 mkfs.vfat drive.img -F 32 -n "PiPS5"
 
+echo "-> Copy supervisord conf"
+cp supervisord.conf /etc/supervisor/conf.d/pips5.conf
+
